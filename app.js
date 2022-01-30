dc7c5e70893ac4a20238f3e4ceae8174364ef6f7
dabdadddy <riposcarblack@gmail.com>
2022-01-29 22:01:06 -0500

    create homepage

--- app.js
+++ app.js
@@ -9,4 +9,10 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.render('pages/home');
+});
 
+app.listen(3000, () => {
+    console.log('Serving on port 3000');
+})
