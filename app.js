a62b14856c1208500c32f2c282e0d34c67b180fa
dabdadddy <riposcarblack@gmail.com>
2022-01-30 21:39:13 -0500

    add partial pages and navbar

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.get('/', (req, res) => {
     res.render('pages/home');
 });
 
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something went wrong';
+    res.status(statusCode).render('error', { err });
+});
+
 app.listen(3000, () => {
     console.log('Serving on port 3000');
 })
